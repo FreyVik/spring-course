18a0719db14297c7478fed39f9547673ec9eb91c
FreyVik <yonkigamer96@gmail.com>
2022-04-14 12:44:28 +0200

    Properties usage

---
+++ src/main/java/com/freyvik/springcourse/MessagesConfig.java
@@ -0,0 +1,13 @@
+package com.freyvik.springcourse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:messages.properties")
+})
+public class MessagesConfig {
+
+}
