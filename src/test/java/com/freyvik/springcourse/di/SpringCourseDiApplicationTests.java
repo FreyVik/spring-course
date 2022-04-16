ef4fb83e77a3557c237fb469a3fb460fcbf58ac5
FreyVik <yonkigamer96@gmail.com>
2022-04-16 20:45:59 +0200

    init push

---
+++ src/test/java/com/freyvik/springcourse/di/SpringCourseDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freyvik.springcourse.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
