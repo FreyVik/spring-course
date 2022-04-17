586b7bc329486c86869241a406e2e8c6f319cba2
FreyVik <yonkigamer96@gmail.com>
2022-04-17 14:43:29 +0200

    Forms validation

--- src/main/java/com/freyvik/springcourse/form/controllers/FormController.java
+++ src/main/java/com/freyvik/springcourse/form/controllers/FormController.java
@@ -1,10 +1,17 @@
 package com.freyvik.springcourse.form.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.freyvik.springcourse.form.domain.Usuario;
 
 @Controller
 public class FormController {
@@ -12,21 +19,33 @@ public class FormController {
 	@GetMapping("/form")
 	public String form(Model model) {
 		model.addAttribute("title", "Form");
+		model.addAttribute("usuario", new Usuario());
 		
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(Model model, 
-			@RequestParam String username,
-			@RequestParam String password,
-			@RequestParam String email) {
+	public String procesar(@Valid Usuario user, BindingResult result, Model model 
+//			@RequestParam String username,
+//			@RequestParam String password,
+//			@RequestParam String email
+			) {		
+		// Getting values from @RequestParam
+//		Usuario user = new Usuario(username, password, email);
 		
 		model.addAttribute("title", "Form resuelt");
-		model.addAttribute("username", username);
-		model.addAttribute("password", password);
-		model.addAttribute("email", email);
 		
+		if (result.hasErrors()) {
+			Map<String, String> errors = new HashMap<>();
+			result.getFieldErrors().forEach(err -> {
+				errors.put(err.getField(), "El campo '".concat(err.getField()).concat("' ").concat(err.getDefaultMessage()));
+			});
+			
+			model.addAttribute("error", errors);			
+			return "form";
+		}		
+		
+		model.addAttribute("user", user);		
 		return "resultado";
 	}
 }
---
+++ src/main/java/com/freyvik/springcourse/form/domain/Usuario.java
@@ -0,0 +1,39 @@
+package com.freyvik.springcourse.form.domain;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Usuario {
+
+	@NotEmpty
+	private String username, password, email;
+	
+	public Usuario() {
+	}
+
+	public Usuario(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}	
+}
