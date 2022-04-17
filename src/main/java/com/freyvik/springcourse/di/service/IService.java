18fc1d80fc3248cf7d6ef31a1f97fa7ff45afca1
FreyVik <yonkigamer96@gmail.com>
2022-04-17 14:02:33 +0200

    formularios

--- src/main/java/com/freyvik/springcourse/di/service/IService.java
+++
@@ -1,6 +0,0 @@
-package com.freyvik.springcourse.di.service;
-
-public interface IService {
-	
-	public String operacion();
-}
--- src/main/java/com/freyvik/springcourse/form/SpringCourseFormApplication.java
+++ src/main/java/com/freyvik/springcourse/form/SpringCourseFormApplication.java
@@ -1,13 +1,13 @@
-package com.freyvik.springcourse.di;
+package com.freyvik.springcourse.form;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCourseDiApplication {
+public class SpringCourseFormApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCourseDiApplication.class, args);
+		SpringApplication.run(SpringCourseFormApplication.class, args);
 	}
 
 }
